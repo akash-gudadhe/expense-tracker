22b773a135debcd28c165adc0fc51c7384ee6d06
akashgudadhe <akashgudadhe112@gmail.com>
2023-10-16 16:30:12 +0530

    add database related details

--- SimpleExpense/src/main/java/com/expensetracker/simpleexpense/repository/UserRepository.java
+++ SimpleExpense/src/main/java/com/expensetracker/simpleexpense/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.expensetracker.simpleexpense.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.expensetracker.simpleexpense.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     
 }
