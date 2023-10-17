d952c44d54ebc2d40f864871ac6bc7eefe9d2409
akashgudadhe <akashgudadhe112@gmail.com>
2023-10-17 19:23:14 +0530

    enable logging configurations

---
+++ SimpleExpense/src/main/java/com/expensetracker/simpleexpense/config/DBConfig.java
@@ -0,0 +1,22 @@
+// package com.expensetracker.simpleexpense.config;
+
+// import javax.sql.DataSource;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+// @Configuration
+// public class DBConfig {
+    
+//     @Bean
+//     public DataSource h2DataSource(){
+//         DriverManagerDataSource datasource = new DriverManagerDataSource();
+//         datasource.setUrl("jdbc:h2:mem:testexpense");
+//         datasource.setDriverClassName("org.h2.Driver");
+//         datasource.setUsername("tempuser");
+//         datasource.setPassword("temppass");
+//         return datasource;
+//     }
+    
+// }
--- SimpleExpense/src/main/java/com/expensetracker/simpleexpense/controller/ExpenseController.java
+++ SimpleExpense/src/main/java/com/expensetracker/simpleexpense/controller/ExpenseController.java
@@ -1,9 +1,16 @@
 package com.expensetracker.simpleexpense.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,26 +27,31 @@ import com.expensetracker.simpleexpense.service.UserService;
 @RequestMapping("/expensetracker/")
 @RestController
 public class ExpenseController {
-
+    private static final Logger logger = LoggerFactory.getLogger(ExpenseController.class);
+    
     @Autowired
     private UserService userService;
 
     @Autowired
     private TransactionService transactionService;
-   
+
 
     @GetMapping(value = "/health")
     public String health(){
+        MDC.put("mdcuserId", "akash");
+        logger.info("Application is running.");
+        MDC.remove("mdcuserId");
         return "OK";
     }
   
+    //TODO: make it secure
     // @Cacheable(value = "allUserTransactions")
     @GetMapping(value = "transactions")
     public Object getAllTransactions(){
         return transactionService.getAllTransactions();
     }
 
-    //transaction based on user id
+    //TODO: make it secure
     // @Cacheable(value = "transactionCache", key="#userId")
     @GetMapping(value = "transactions/{userId}")
     public ResponseEntity<?> getAllTransactionsOfUser(@PathVariable("userId") Long uId){
@@ -52,9 +64,27 @@ public class ExpenseController {
         return userTransactions.isEmpty() ? ResponseEntity.status(HttpStatus.NOT_FOUND).body("No Transactions found for user"+uId) : ResponseEntity.ok(userTransactions);
     }
 
+
     @GetMapping("user/{userId}")
     public String userDetails(@PathVariable Long userId){
         return userService.userExists(userId) ? "User Exists" : "UserNotFound";
+    }
+
+    @GetMapping("datasource")
+    public String dataSourceData(){
+        // try{
+        //     Connection connectionDs1 = datasource.getConnection();
+        //     String sqlQuery = "SELECT * FROM Transaction";
+        //     PreparedStatement ps = connectionDs1.prepareStatement(sqlQuery);
+        //     ResultSet result = ps.executeQuery();
+
+        //     while(result.next()){
+        //         System.out.println(result.getLong("id"));
+        //     }
+        // }catch(SQLException e){
+        //     e.printStackTrace();
+        // }
+        return null;
     }
 
 }
